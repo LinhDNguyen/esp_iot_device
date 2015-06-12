a5443be82192ec5c2f3f2d6a24d00832f5f6a082
Linh Nguyen <B51960@freescale.com>
2015-06-12 12:54:13 +0700

    Run OK with smartconfig

--- include/user_config.h
+++ include/user_config.h
@@ -4,5 +4,13 @@
 #define WIFI_SSID       "Vinh 1102"
 #define WIFI_PASSWORD   "22446688"
 
+#define CONFIG_SECTOR_NUM   0x7E
+#define CONFIG_ADDRESS      0x7E000
+
+#define STATE_UNKNOWN       0U
+#define STATE_START         1U
+#define STATE_NETCONNECTED  2U
+#define STATE_MAX           3U
+
 #endif
 
--- user/user_main.c
+++ user/user_main.c
@@ -4,6 +4,7 @@
 #include "os_type.h"
 #include "user_config.h"
 #include "user_interface.h"
+#include "smartconfig.h"
 #include "espconn.h"
 
 #define user_procTaskPrio        0
@@ -11,8 +12,53 @@
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void loop(os_event_t *events);
 
+// Global VARs
+static uint8 curState = STATE_UNKNOWN;
+static volatile os_timer_t curTimer;
+
+// Functions
 void user_rf_pre_init(void)
 {
+}
+
+void ICACHE_FLASH_ATTR
+smartconfig_done(sc_status status, void *pdata)
+{
+    uint8 phone_ip[4] = {0};
+    struct station_config *sta_conf = pdata;
+
+    switch(status) {
+        case SC_STATUS_WAIT:
+            os_printf("SC_STATUS_WAIT\n");
+            break;
+        case SC_STATUS_FIND_CHANNEL:
+            os_printf("SC_STATUS_FIND_CHANNEL\n");
+            break;
+        case SC_STATUS_GETTING_SSID_PSWD:
+            os_printf("SC_STATUS_GETTING_SSID_PSWD\n");
+            break;
+        case SC_STATUS_LINK:
+            os_printf("SC_STATUS_LINK\n");
+
+            wifi_station_set_config(sta_conf);
+            wifi_station_disconnect();
+            wifi_station_connect();
+            break;
+        case SC_STATUS_LINK_OVER:
+            os_printf("SC_STATUS_LINK_OVER\n");
+            os_memcpy(phone_ip, (uint8*)pdata, 4);
+            os_printf("Phone ip: %d.%d.%d.%d\n",phone_ip[0],phone_ip[1],phone_ip[2],phone_ip[3]);
+            smartconfig_stop();
+            wifi_station_set_auto_connect(1);
+            break;
+    }
+
+}
+
+// Check the wifi connectivity.
+void timer_check_connection(void *arg)
+{
+    smartconfig_start(SC_TYPE_ESPTOUCH, smartconfig_done);
 }
 
 //Main code function
@@ -30,10 +76,14 @@ void ICACHE_FLASH_ATTR wifi_event_cb(System_Event_t *evt)
         case EVENT_STAMODE_CONNECTED: //When connected to AP, change SoftAP to same channel to keep DHCPS working
         newchannel = evt->event_info.connected.channel;
         os_printf("\r\nWifi connected at channel %d\r\n", newchannel);
+        curState = STATE_START;
         break;
 
         case EVENT_STAMODE_GOT_IP:
         os_printf("\r\nWifi got IP...\r\n");
+        curState = STATE_NETCONNECTED;
+        // Disable timer when connected
+        os_timer_disarm(&curTimer);
         break;
     }
 
@@ -43,8 +93,8 @@ void ICACHE_FLASH_ATTR wifi_event_cb(System_Event_t *evt)
 void ICACHE_FLASH_ATTR
 user_init()
 {
-    char ssid[32] = WIFI_SSID;
-    char password[64] = WIFI_PASSWORD;
+    char ssid[32] = {0};
+    char password[64] = {0};
     struct station_config stationConf;
 
     // Initialize UART0 to use as debug
@@ -55,14 +105,25 @@ user_init()
     // Use GPIO4 led to indicate wifi status
     wifi_status_led_install(4, PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4);
 
+    wifi_station_set_auto_connect(1);
+
     //Set station mode
-    wifi_set_opmode( 0x1 );
+    wifi_set_opmode(STATION_MODE);
+
+    // Check Flash saved configuration
+    wifi_station_get_config_default(&stationConf);
 
     //Set ap settings
-    os_memcpy(&stationConf.ssid, ssid, 32);
-    os_memcpy(&stationConf.password, password, 64);
-    wifi_station_set_config(&stationConf);
+    os_printf("\r\n===%s===%s===", stationConf.ssid, stationConf.password);
+    wifi_station_set_config_current(&stationConf);
     wifi_set_event_handler_cb(wifi_event_cb);
+    wifi_station_set_reconnect_policy(1);
+
+    // Configure the timer to check connection timeout.
+    // After 5s, if wifi is not connected, start the smartconfig
+    os_timer_disarm(&curTimer);
+    os_timer_setfn(&curTimer, (os_timer_func_t *)timer_check_connection, NULL);
+    os_timer_arm(&curTimer, 7000, 0);
 
     //Start os task
     system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
