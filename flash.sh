5428d742493fd034c64e25622f77879208968bfd
Linh Nguyen (B51960) <B51960@freescale.com>
2015-06-23 15:56:00 +0700

    Update flash script

--- flash.sh
+++ flash.sh
@@ -3,25 +3,29 @@ flashrom=0
 flashblank=0
 flashconfig=0
 runuart=0
+usecom=/dev/ttyUSB0
 
-if [[ "x${1}x" != "xx" ]]; then
+if [[ "x${1}x" == "xx" ]]; then
   flashrom=1;
   runuart=1;
 else
-  if [[ `expr index "$1" r` ]]; then
+  if [[ `expr index "$1" r` > 0 ]]; then
     flashrom=1;
   fi
-  if [[ `expr index "$1" u` ]]; then
+  if [[ `expr index "$1" u` > 0 ]]; then
     runuart=1;
+  fi
+  if [[ "x${2}x" != "xx" ]]; then
+    usecom=${2}
   fi
 fi
 
 if [[ $flashrom -gt 0 ]]; then
   echo "============FLASH NEW ROM============"
-  esptool.py --port /dev/ttyUSB0 write_flash 0x00000 ../bin/eagle.flash.bin 0x40000 ../bin/eagle.irom0text.bin;
+  ../esptool.py --port ${usecom} write_flash 0x00000 ../bin/eagle.flash.bin 0x40000 ../bin/eagle.irom0text.bin;
 fi
 
 if [[ $runuart -gt 0 ]]; then
   echo "============RUN UART=============="
-  miniterm.py /dev/ttyUSB0 115200;
+  miniterm.py ${usecom} 115200;
 fi
